d4a7ab48256ca8e0ef2f730104d7fa9d531a11e2
pranjalAg <pranjalag97@gmail.com>
2019-03-12 13:49:03 +0530

    jsadd

---
+++ dir.js
@@ -0,0 +1,9 @@
+function displayform() {
+          document.getElementById("dataenter").style.display = "inline-block"; 
+          document.getElementById("tableview").style.display = "none";    
+}
+
+function displaytable() {
+          document.getElementById("tableview").style.display = "inline-block";
+          document.getElementById("dataenter").style.display = "none";     
+}
