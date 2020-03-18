3c06644bb5e68492d52e82ef6da9c575b29314a3
pranjalAg <pranjalag97@gmail.com>
2020-03-19 02:51:41 +0530

    js files

--- custom.js
+++ custom.js
@@ -1,9 +1,34 @@
-function displayform() {
-          document.getElementById("dataenter").style.display = "inline-block"; 
-          document.getElementById("tableview").style.display = "none";    
-}
+$( document ).ready(function() {
+	var list = '<td style="text-align:center;" colspan="4">No record</td>'
+	$('.NoRecordList').html(list);
+	$( ".addNewrecord" ).click(function() {
+		$('#testmodal').modal('show');
+		// $("#dataenter").css("display","inline-block");
+		// $("#dataenter").slideToggle(); 
+	});
+	$( ".confirm_appointment" ).submit(function( event ) {
+			  event.preventDefault();
+			  var record = $( this ).serializeArray();
+			  var list = '<tr id="Reco_">';
+			  var list2 = '';
+			   $.each(record, function (index, value) {
+			   		list2 = list2+'<td>'+value.value+'</td>';
 
-function displaytable() {
-          document.getElementById("tableview").style.display = "inline-block";
-          document.getElementById("dataenter").style.display = "none";     
-}
+			    });
+			   var list = list+list2 +'<td><button class="btn-danger">Delete</button></td></tr>';
+			   $('.NoRecordList').remove();
+			   $('.RecordList').append(list);
+			   	$('#testmodal').modal('hide');
+
+
+	});
+ //    function displayform() {
+ //          document.getElementById("dataenter").style.display = "inline-block"; 
+ //          document.getElementById("tableview").style.display = "none";    
+	// };
+
+	// function displaytable() {
+	//           document.getElementById("tableview").style.display = "inline-block";
+	//           document.getElementById("dataenter").style.display = "none";     
+	// }
+});
