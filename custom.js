dea26300be17a9c3d0f8f708ea9408f8e2a2788f
pranjalAg <pranjalag97@gmail.com>
2020-03-20 23:11:23 +0530

    dir

--- custom.js
+++ custom.js
@@ -1,34 +1,32 @@
 $( document ).ready(function() {
 	var list = '<td style="text-align:center;" colspan="4">No record Found !!</td>'
 	$('.NoRecordList').html(list);
-	$( ".addNewrecord" ).click(function() {
+	/* Function to open pop-up
+	*/
+	$(".addNewrecord").click(function() {
 		$('#testmodal').modal('show');
-		// $("#dataenter").css("display","inline-block");
-		// $("#dataenter").slideToggle(); 
 	});
-	$( ".confirm_appointment" ).submit(function( event ) {
-			  event.preventDefault();
-			  var record = $( this ).serializeArray();
-			  var list = '<tr id="Reco_">';
-			  var list2 = '';
-			   $.each(record, function (index, value) {
-			   		list2 = list2+'<td>'+value.value+'</td>';
-
-			    });
-			   var list = list+list2 +'<td><button class="btn-danger">Delete</button></td></tr>';
-			   $('.NoRecordList').remove();
-			   $('.RecordList').append(list);
-			   	$('#testmodal').modal('hide');
-
-
+	/* Function to Submit Form and add record
+	*/
+	$(".confirm_appointment").submit(function( event ) {
+		event.preventDefault();
+		var record = $( this ).serializeArray();
+		var count = parseInt($('input[name="recordcount"]').val())+1;
+		$('input[name="recordcount"]').val(count);
+		var list = '<tr id="Reco_'+count+'">';
+		var list2 = '';
+		$.each(record, function (index, value) {
+			list2 = list2+'<td>'+value.value+'</td>';
+		});
+		var list = list+list2 +'<td><button id='+count+' class="btn deleteRec btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button></td></tr>';
+		$('.NoRecordList').remove();
+		$('.RecordList').append(list);
+		$('#testmodal').modal('hide');
 	});
- //    function displayform() {
- //          document.getElementById("dataenter").style.display = "inline-block"; 
- //          document.getElementById("tableview").style.display = "none";    
-	// };
-
-	// function displaytable() {
-	//           document.getElementById("tableview").style.display = "inline-block";
-	//           document.getElementById("dataenter").style.display = "none";     
-	// }
+	/* Function to delete Record
+	*/
+	$(document).on("click", ".deleteRec", function(){
+		var recid = $(this).attr('id');
+		$('#Reco_'+recid).remove();
+	});
 });
